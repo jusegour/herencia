55f13d23198e41643fc528069239018ba77e7b06
jusegour <jusegour2015@gmail.com>
2020-12-10 11:27:49 -0500

    Codigo de constructor PersonaManager y config.propierties, corregidos.

--- src/main/java/com/clearminds/impl/PersonaManager.java
+++ src/main/java/com/clearminds/impl/PersonaManager.java
@@ -1,5 +1,8 @@
 package com.clearminds.impl;
 
+import java.io.FileReader;
+import java.util.Properties;
+
 import com.clearminds.exepciones.InstanceException;
 import com.clearminds.interfaces.ServicioPersona;
 import com.clearminds.model.Persona;
@@ -8,13 +11,18 @@ public class PersonaManager {
 
 	private ServicioPersona serv;
 
-	public PersonaManager(String nombreClase) throws InstanceException {
+	public PersonaManager() throws InstanceException {
 		super();
-
-		String clasePorCargar = "com.clearminds.impl." + nombreClase;
-
 		try {
-			Class<?> clase = Class.forName(clasePorCargar);
+			Properties propiedades = new Properties();
+			String clasePorCargar = "";
+			String paquete = "";
+
+			propiedades.load(new FileReader("config.properties"));
+			clasePorCargar = propiedades.getProperty("ClaseServicio");
+			paquete = propiedades.getProperty("paquete");
+
+			Class<?> clase = Class.forName(paquete + clasePorCargar);
 			serv = (ServicioPersona) clase.newInstance();
 		} catch (Exception e) {
 			throw new InstanceException("Error al obtener una instancia de ServicioPersona");
